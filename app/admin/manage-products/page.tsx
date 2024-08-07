535427752d73097bcd690907fa107a133742d1f8
Vuyo19 <93484408+Vuyo19@users.noreply.github.com>
2024-08-07 23:03:52 +0200

    thirteenth commit

--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,30 +1,55 @@
-
 import Container from "@/app/components/Container";
 import NullData from "@/app/components/NullData";
 import React from 'react'
 import ManageProductsClient from "./ManageProductsClient";
 import getProducts from "@/actions/getProducts"; 
 import { getCurrentUser } from "@/actions/getCurrentUser";
+import prisma from '@/libs/prismadb';
+import { GetServerSideProps } from "next";
+import { Product } from '@prisma/client';
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { category } = context.query;
 
-const ManageProducts = async() => { 
-
-  // Retrieving the products from MongoDB. 
-
-  const products = await getProducts({category: null}); 
-  const currentUser = await getCurrentUser(); 
-
-  if(!currentUser || currentUser.role !== 'ADMIN') {
-    return <NullData title="Oops! Access denied" />
+  if (!category || typeof category !== 'string') {
+    return {
+      notFound: true,
+    };
   }
 
-  return (
-    <div className="pt-8">
-        <Container> 
-            <ManageProductsClient products={products} /> 
-        </Container>
-    </div>
-  )
+  try {
+    // Fetch data based on the category
+    const data = await prisma.product.findMany({
+      where: {
+        category: category,
+      },
+    });
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return {
+      notFound: true,
+    };
+  }
+}; 
+
+interface ManageProductsPageProps {
+  data: Product[];
 }
 
-export default ManageProducts; 
+const ManageProductsPage: React.FC<ManageProductsPageProps> = ({ data }) => {
+  return (
+    <div className="pt-8">
+      <Container>
+        <ManageProductsClient products={data} />
+      </Container>
+    </div>
+  );
+};
+
+export default ManageProductsPage;
