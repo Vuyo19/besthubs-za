b2782c59e37b976cced774f2965baf0e52a418b8
Vuyo19 <93484408+Vuyo19@users.noreply.github.com>
2024-08-07 22:37:24 +0200

    ninth commit

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -1,21 +1,17 @@
-'use client'
-
 import { getCurrentUser } from "@/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/libs/prismadb"
 
-export async function DELETE(request: Request, {params}: {params: {id: string}}) { 
-    
-    const currentUser = await getCurrentUser(); 
-    
-    if(!currentUser || currentUser.role !== 'ADMIN') {
-        return NextResponse.error(); 
-    }  
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser || currentUser.role !== 'ADMIN') {
+        return NextResponse.error();
+    }
 
     const product = await prisma?.product.delete({
-        where: {id: params.id}
-    })
+        where: { id: params.id }
+    });
 
-    return NextResponse.json(product); 
-
-} 
+    return NextResponse.json(product);
+}
